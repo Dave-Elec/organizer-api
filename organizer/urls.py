06ff9d966dd2560c4ea611003021e387e70b206c
Dawit Abate <dawitabate2@gmail.com>
2021-11-13 13:52:49 +0300

    Added task lists

--- organizer/urls.py
+++ organizer/urls.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("users/", include("users.urls", namespace="users")),
+    path("", include("todo.urls", namespace="todo")),
     path("auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("auth/token/", obtain_auth_token),
 ]
